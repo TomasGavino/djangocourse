163b10249d8d2fca26f0ff789c225a4158cf6452
tomas <tomas@eterny.ai>
2019-11-02 08:29:46 -0600

    create user and superuser

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email adress')
-
         user=self.model(email = self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using = self._db)
@@ -23,11 +19,11 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using = self._db)
-
+        
         return user
 
-
 class User(AbstractBaseUser,PermissionsMixin):
+
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length = 255, unique = True)
     name = models.CharField(max_length = 255)
@@ -35,5 +31,3 @@ class User(AbstractBaseUser,PermissionsMixin):
     is_staff = models.BooleanField(default = False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
-                                        
-
